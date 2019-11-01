fd329938922acd0c91f6e263d057cfa5a614c724
Liuyong <huochex@sohu.com>
2019-11-01 11:37:52 +0800

    增加双击退出和注销

--- restaurant/src/main/java/com/goldenstraw/restaurant/goodsmanager/Main2Activity.kt
+++ restaurant/src/main/java/com/goldenstraw/restaurant/goodsmanager/Main2Activity.kt
@@ -1,21 +1,22 @@
 package com.goldenstraw.restaurant.goodsmanager
 
 import android.content.Intent
-import android.os.Bundle
+import android.view.KeyEvent
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import arrow.instance
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.goldenstraw.restaurant.R
 import com.goldenstraw.restaurant.databinding.ActivityMain2Binding
 import com.goldenstraw.restaurant.goodsmanager.ui.check.CheckQuantityActivity
 import com.goldenstraw.restaurant.goodsmanager.ui.goods.OrderManagerActivity
-import com.goldenstraw.restaurant.goodsmanager.ui.verify.VerifyAndPlaceOrderActivity
 import com.goldenstraw.restaurant.goodsmanager.ui.query.QueryOrdersActivity
 import com.goldenstraw.restaurant.goodsmanager.ui.record.RecordOrdersActivity
 import com.goldenstraw.restaurant.goodsmanager.ui.supplier.SupplierApplyActivity
+import com.goldenstraw.restaurant.goodsmanager.ui.verify.VerifyAndPlaceOrderActivity
 import com.goldenstraw.restaurant.goodsmanager.utils.PrefsHelper
 import com.owner.basemodule.base.view.activity.BaseActivity
+import com.owner.basemodule.util.toast
 import kotlinx.android.synthetic.main.activity_main2.*
 import org.kodein.di.Copy
 import org.kodein.di.Kodein
@@ -35,6 +36,7 @@ class Main2Activity : BaseActivity<ActivityMain2Binding>() {
     }
     private val prefs: PrefsHelper by instance()
     override fun initView() {
+        setSupportActionBar(main_toolbar)
         hideAllFunction()
         when (prefs.role) {
             "供应商" -> {
@@ -93,12 +95,49 @@ class Main2Activity : BaseActivity<ActivityMain2Binding>() {
         }
     }
 
-    fun hideAllFunction() {
+    private fun hideAllFunction() {
         order.visibility = View.GONE
         send.visibility = View.GONE
         query.visibility = View.GONE
         supplier.visibility = View.GONE
         check.visibility = View.GONE
         record.visibility = View.GONE
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_logout_user, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item!!.itemId) {
+
+            R.id.logout_user -> {
+
+                prefs.autoLogin = false
+            }
+
+        }
+        finish()
+        return true
+    }
+
+    /**
+     * 实现再按一次返回键退出程序
+     */
+    private var exitTime: Long = 0
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_DOWN) {
+            if (System.currentTimeMillis() - exitTime > 3000) {
+                toast { "再按一次退出程序！" }
+                exitTime = System.currentTimeMillis()
+            } else {
+                finish()
+            }
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
--- restaurant/src/main/java/com/goldenstraw/restaurant/goodsmanager/utils/PrefsHelper.kt
+++ restaurant/src/main/java/com/goldenstraw/restaurant/goodsmanager/utils/PrefsHelper.kt
@@ -1,9 +1,9 @@
 package com.goldenstraw.restaurant.goodsmanager.utils
 
 import android.content.SharedPreferences
+import com.owner.basemodule.util.prefs.boolean
 import com.owner.basemodule.util.prefs.int
 import com.owner.basemodule.util.prefs.string
-import com.owner.basemodule.util.prefs.stringSet
 
 /**
  * Created by Administrator on 2019/10/30 0030
@@ -19,6 +19,8 @@ class PrefsHelper(prefs: SharedPreferences) {
     var role by prefs.string("role", "")
 
     var rights by prefs.string("rights", "")
+
+    var autoLogin by prefs.boolean("autoLogin", false)
 
 
 }
